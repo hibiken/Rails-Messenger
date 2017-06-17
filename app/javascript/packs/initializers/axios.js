9a512d63c25765e5a14dfd5ce1858b3e92174817
Ken Hibino <ken.hibino7@gmail.com>
2017-06-17 11:08:58 -0700

    Update axios initializer to use correct baseURL in production

--- app/javascript/packs/initializers/axios.js
+++ app/javascript/packs/initializers/axios.js
@@ -1,9 +1,12 @@
 import axios from 'axios';
 
 const authToken = document.head.querySelector('[name="jwt-token"]').content
+const baseURL = process.env.NODE_ENV === 'production' ?
+                'https://salty-beyond-48780.herokuapp.com' :
+                'http://localhost:3000';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:3000',
+  baseURL,
   headers: {
     'Authorization': `Token ${authToken}`,
   },
