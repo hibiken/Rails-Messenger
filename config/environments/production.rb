37c99e9be2ffafd0fea4ddebb524e20e8a2374a1
Ken Hibino <ken.hibino7@gmail.com>
2017-06-19 08:48:14 -0700

    Update ActionCable configuration for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -41,8 +41,8 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
-  # config.action_cable.url = 'wss://example.com/cable'
-  # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
+  config.action_cable.url = 'wss://salty-beyond-48780.herokuapp.com/cable'
+  config.action_cable.allowed_request_origins = ['https://salty-beyond-48780.herokuapp.com', 'http://salty-beyond-48780.herokuapp.com' ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
