58b5df3db363fa6f6d4d1aef3e170e75dc1dbbe4
Ken Hibino <ken.hibino7@gmail.com>
2017-08-01 08:36:10 -0700

    Move route redirect logic to Dashboard

--- app/javascript/packs/containers/dashboard.jsx
+++ app/javascript/packs/containers/dashboard.jsx
@@ -1,10 +1,41 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
+import {
+  fetchMessageThreads,
+  setActiveThread,
+  fetchMessagesFor,
+} from '../actions/message_threads';
+import {
+  getAllMessageThreads,
+  getMostRecentMessageThreadId,
+  getActiveMessageThreadId,
+  getIsAddingNewThread,
+  getIsNewThreadActive,
+  getNewMessageThreadUsers,
+} from '../selectors';
 import MainSidebarContainer from './main_sidebar_container';
 import MessageDetailViewContainer from './message_detail_view_container';
 
 class Dashboard extends Component {
+  componentWillMount() {
+    this.props.fetchMessageThreads()
+      .then(() => {
+        const { activeThreadId, mostRecentThreadId } = this.props;
+        if (activeThreadId !== null) {
+          this.props.setActiveThread(activeThreadId);
+          this.props.fetchMessagesFor(activeThreadId);
+          return;
+        }
+
+        if (mostRecentThreadId !== false) {
+          this.props.setActiveThread(mostRecentThreadId);
+          this.props.fetchMessagesFor(mostRecentThreadId);
+        }
+      });
+  }
+
   render() {
     return (
       <Router>
@@ -17,4 +48,20 @@ class Dashboard extends Component {
   }
 }
 
-export default Dashboard;
+const mapStateToProps = (state) => ({
+  isAddingNewThread: getIsAddingNewThread(state),
+  isNewThreadActive: getIsNewThreadActive(state),
+  newMessageThreadUsers: getNewMessageThreadUsers(state),
+  messageThreads: getAllMessageThreads(state),
+  activeThreadId: getActiveMessageThreadId(state),
+  mostRecentThreadId: getMostRecentMessageThreadId(state),
+});
+
+export default connect(
+  mapStateToProps,
+  {
+    fetchMessageThreads,
+    setActiveThread,
+    fetchMessagesFor,
+  }
+)(Dashboard);
--- app/javascript/packs/containers/main_sidebar_container.jsx
+++ app/javascript/packs/containers/main_sidebar_container.jsx
@@ -2,51 +2,13 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import MainSidebar from '../components/main_sidebar';
-import { withRouter } from 'react-router-dom';
-import {
-  fetchMessageThreads,
-  setActiveThread,
-  fetchMessagesFor,
-} from '../actions/message_threads';
 import {
   getAllMessageThreads,
-  getMostRecentMessageThreadId,
   getActiveMessageThreadId,
   getIsAddingNewThread,
   getIsNewThreadActive,
   getNewMessageThreadUsers,
 } from '../selectors';
-
-class MainSidebarContainer extends Component {
-  componentWillMount() {
-    this.props.fetchMessageThreads()
-      .then(() => {
-        const { activeThreadId, mostRecentThreadId } = this.props;
-        if (activeThreadId !== null) {
-          this.props.setActiveThread(activeThreadId);
-          this.props.fetchMessagesFor(activeThreadId);
-          return;
-        }
-
-        if (mostRecentThreadId !== false) {
-          this.props.setActiveThread(mostRecentThreadId);
-          this.props.fetchMessagesFor(mostRecentThreadId);
-        }
-      });
-  }
-
-  render() {
-    return (
-      <MainSidebar
-        isAddingNewThread={this.props.isAddingNewThread}
-        isNewThreadActive={this.props.isNewThreadActive}
-        newMessageThreadUsers={this.props.newMessageThreadUsers}
-        messageThreads={this.props.messageThreads}
-        activeThreadId={this.props.activeThreadId}
-      />
-    );
-  }
-};
 
 const mapStateToProps = (state) => ({
   isAddingNewThread: getIsAddingNewThread(state),
@@ -54,14 +16,6 @@ const mapStateToProps = (state) => ({
   newMessageThreadUsers: getNewMessageThreadUsers(state),
   messageThreads: getAllMessageThreads(state),
   activeThreadId: getActiveMessageThreadId(state),
-  mostRecentThreadId: getMostRecentMessageThreadId(state),
 });
 
-export default connect(
-  mapStateToProps,
-  {
-    fetchMessageThreads,
-    setActiveThread,
-    fetchMessagesFor,
-  }
-)(MainSidebarContainer);
+export default connect(mapStateToProps)(MainSidebar);
