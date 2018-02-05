a2e4a7333952f93ff686c5abe1a58dbe379f809d
Ken Hibino <ken.hibino7@gmail.com>
2018-02-04 16:43:46 -0800

    Update production base url

--- app/javascript/initializers/axios.js
+++ app/javascript/initializers/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const authToken = document.head.querySelector('[name="jwt-token"]').content;
 const baseURL = process.env.NODE_ENV === 'production' ?
-                'https://salty-beyond-48780.herokuapp.com' :
+                'https://rails-messenger.herokuapp.com' :
                 'http://localhost:3000';
 
 const instance = axios.create({
